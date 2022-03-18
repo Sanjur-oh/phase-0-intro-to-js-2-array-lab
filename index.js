81a5d0693ed7842ac48aa54b950c77fcfbe7e1f8
Michael Franco <franco.luis.michael@gmail.com>
2022-03-18 11:20:29 -0500

    commit tree 56e4289a9c93dfbbf2fd049d75a05f50e7da5125

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+// const cats = ["Milo", "Otis", "Garfield"];
+
+// cats.push("Ralph");
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newArray = [...cats]
+    newArray.push(name);
+    return newArray;
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1)
+}
+
+function removeFirstCat() {
+   return cats.slice(1);
+}
